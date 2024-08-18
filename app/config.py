cea9beefba863a02b31e255d2b667120fc29a67d
AmmarDaruger <159748948+s223201882@users.noreply.github.com>
2024-08-18 22:12:06 +1000

    undo

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Settings:
-    DATABASE_URL: str = os.getenv("postgresql://sit722_part1_db_32nf_user:82hFDixW65kmS40VgupdxYXaRy7tnJK3@dpg-cr0tvsbqf0us73ffsqlg-a.oregon-postgres.render.com/sit722_part1_db_32nf")
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
 
 settings = Settings()
